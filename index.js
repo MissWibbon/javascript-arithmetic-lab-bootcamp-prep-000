a3ffdb18aa65d2681bdb702221c288d6f4abd86a
Michelle Wuebben <wuebbenmichelle@gmail.com>
2019-05-02 10:27:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var number = 5
+
+function add(){
+  number + 2
+}
+console.log(number)
+function subtract(){
+  
+}
+function multiply(){
+  
+}
+function divide(){
+  
+}
+function inc(){
+  
+}
+function dec(){
+  
+}
+function makeInt(){
+  
+}
+function preserveDecimal(){
+  
+}
